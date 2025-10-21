e11a4fe1e174833e69c956c484545b482539cfa5
andreluislima <andrelimag89@gmail.com>
2025-10-21 12:31:30 -0300

    chore(init): estrutura inicial do projeto

---
+++ src/main/java/com/api/payment/PyamentGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyamentGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyamentGatewayApiApplication.class, args);
+	}
+
+}
