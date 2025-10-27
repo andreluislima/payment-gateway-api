0b11b34f1b1c9690bd7b23440e797d039b691618
andreluislima <andrelimag89@gmail.com>
2025-10-27 14:54:12 -0300

    feat(test): cria teste registra no domain

---
+++ src/test/java/com/andrelima/api/payment/domain/CobrancaTest.java
@@ -0,0 +1,37 @@
+package com.andrelima.api.payment.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.api.payment.domain.Cobranca;
+import com.api.payment.enums.StatusCobranca;
+
+public class CobrancaTest {
+
+	@Test
+	void definirValoresCorretamente (){
+		Cobranca cobranca = new Cobranca();
+		cobranca.setCpfDestinatario("12345678900");
+		cobranca.setDescricao("Servico prestado");
+		cobranca.setValorCobranca(150.00);
+		cobranca.setStatus(StatusCobranca.PAGA);
+		
+		assertEquals("12345678900", cobranca.getCpfDestinatario());
+		assertEquals("Servico prestado", cobranca.getDescricao());
+		assertEquals(150.00, cobranca.getValorCobranca());
+		assertEquals(StatusCobranca.PAGA, cobranca.getStatus());
+	}
+	                                    
+	@Test
+	void terStatusPadraoPendente() {
+		Cobranca cobranca = new Cobranca();
+		assertEquals(StatusCobranca.PENDENTE, cobranca.getStatus());
+	}
+}
+
+
+
+
+
+
